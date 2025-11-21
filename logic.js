3335ae06c7f8a0effd840d91742f8280039dae81
mohamedredachakir <chakirm082@gmail.com>
2025-11-21 16:32:53 +0100

    strecture de function renderreception

--- logic.js
+++ logic.js
@@ -342,3 +342,40 @@ function rendersecurite() {
 }
 
 rendersecurite();
+
+
+
+function renderreception(){
+    Receptionplace.innerHTML = '';
+   Reception.forEach((user,index)=>{
+    const card = document.createElement('div');
+    card.className = card.className ="flex justify-evenly items-center w-full h-[40px] bg-white/80 backdrop-blur-sm shadow-md border border-gray-300 rounded-xl hover:shadow-lg transition duration-200";
+    card.innerHTML = `
+     <button class="infoinplace"><h2 class="text-center w-[50%] text-[12px] font-bold">${user.name}</h2></button>
+     <button class="deleteinplace bg-red-600 w-fit text-white px-3 py-1 rounded-lg text-[12px] hover:bg-red-700 transition">X</button>
+    `
+    
+    const infoinplace = card.querySelector('.infoinplace');
+    
+    infoinplace.addEventListener('click',()=>{
+        Receptioninfo(index)
+    })
+    
+    const deleteinplace = card.querySelector('.deleteinplace');
+    
+    deleteinplace.addEventListener('click',()=>{
+        let deleteIndex = Reception.findIndex(u => u.id === user.id)
+        let deletedUser = Reception.splice(deleteIndex,1);
+        allusers.push(deletedUser[0]);
+        localStorage.setItem('Reception',JSON.stringify(Reception));
+        localStorage.setItem('allusers',JSON.stringify(allusers));
+        console.log(allusers);
+        renderreception();
+        displayall()
+    })
+    Receptionplace.appendChild(card);
+})
+
+}
+
+renderreception();
