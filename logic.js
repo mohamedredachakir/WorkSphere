8c43153cced73aab8b94f51bcc7b1d0be94e7825
mohamedredachakir <chakirm082@gmail.com>
2025-11-21 16:34:28 +0100

    strecture de function renderconference

--- logic.js
+++ logic.js
@@ -379,3 +379,42 @@ function renderreception(){
 }
 
 renderreception();
+
+function renderconfernce() {
+    conferenceplace.innerHTML = '';
+
+    conference.forEach((user, index) => {
+        const card = document.createElement('div');
+        card.className =
+            "flex justify-evenly items-center w-full h-[40px] bg-white/80 backdrop-blur-sm shadow-md border border-gray-300 rounded-xl hover:shadow-lg transition duration-200";
+
+        card.innerHTML = `
+            <button class="infoinplace">
+                <h2 class="text-center w-[50%] text-[12px] font-bold">${user.name}</h2>
+            </button>
+            <button class="deleteinplace bg-red-600 w-fit text-white px-3 py-1 rounded-lg text-[12px] hover:bg-red-700 transition">X</button>
+        `;
+
+        const infoinplace = card.querySelector('.infoinplace');
+        infoinplace.addEventListener('click', () => {
+            conferenceinfo(index);
+        });
+
+        const deleteinplace = card.querySelector('.deleteinplace');
+        deleteinplace.addEventListener('click', () => {
+            let deleteIndex = conference.findIndex(u => u.id === user.id)
+            let deletedUser = conference.splice(deleteIndex,1);
+            allusers.push(deletedUser[0]);
+
+            localStorage.setItem('conference', JSON.stringify(conference));
+            localStorage.setItem('allusers', JSON.stringify(allusers));
+
+            renderconfernce();
+            displayall();
+        });
+
+        conferenceplace.appendChild(card);
+    });
+}
+
+renderconfernce();
