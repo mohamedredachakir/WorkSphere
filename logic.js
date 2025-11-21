03d091363f80caaafa7ef5496b07d3032dc6b6cc
mohamedredachakir <chakirm082@gmail.com>
2025-11-21 12:57:55 +0100

    les 1ere variables

---
+++ logic.js
@@ -0,0 +1,16 @@
+const btnadd = document.querySelector("#bntadd");
+const addplace = document.querySelector("#addplace");
+const modalform = document.querySelector("#modalform");
+const addForm = document.querySelector("#add-form");
+const btnannuler = document.querySelector("#btnannuler");
+const btnexpr = document.querySelector("#btnexpr");
+
+const username = document.querySelector("#username");
+const userage = document.querySelector("#userage");
+const usermail = document.querySelector("#usermail");
+const usertel = document.querySelector("#usertel");
+const userrole = document.querySelector("#userrole");
+const userimg = document.querySelector("#userimg");
+const userstart = document.querySelector("#userstart");
+const userleave = document.querySelector("#userleave");
+const btnsubmit = document.querySelector("#btnsubmit");
