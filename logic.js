4f294fd71cfcdc5f5fe3f9cdd5dde01f240c803d
mohamedredachakir <chakirm082@gmail.com>
2025-11-21 16:35:53 +0100

    strecture de function renderarchive

--- logic.js
+++ logic.js
@@ -457,3 +457,43 @@ function renderstaff() {
 }
 
 renderstaff();
+
+
+function renderarchive() {
+    archiveplace.innerHTML = '';
+
+    archive.forEach((user, index) => {
+        const card = document.createElement('div');
+        card.className =
+            "flex justify-evenly items-center w-full h-[40px] bg-white/80 backdrop-blur-sm shadow-md border border-gray-300 rounded-xl hover:shadow-lg transition duration-200";
+
+        card.innerHTML = `
+            <button class="infoinplace">
+                <h2 class="text-center w-[50%] text-[12px] font-bold">${user.name}</h2>
+            </button>
+            <button class="deleteinplace bg-red-600 w-fit text-white px-3 py-1 rounded-lg text-[12px] hover:bg-red-700 transition">X</button>
+        `;
+
+        const infoinplace = card.querySelector('.infoinplace');
+        infoinplace.addEventListener('click', () => {
+            archiveinfo(index);
+        });
+
+        const deleteinplace = card.querySelector('.deleteinplace');
+        deleteinplace.addEventListener('click', () => {
+            let deleteIndex = archive.findIndex(u => u.id === user.id)
+            let deletedUser = archive.splice(deleteIndex,1);
+            allusers.push(deletedUser[0]);
+
+            localStorage.setItem('archive', JSON.stringify(archive));
+            localStorage.setItem('allusers', JSON.stringify(allusers));
+
+            renderarchive();
+            displayall();
+        });
+
+        archiveplace.appendChild(card);
+    });
+}
+
+renderarchive();
